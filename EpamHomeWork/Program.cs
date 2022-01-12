ddb33ce08f9c83deb5ab4835aaf68eed441d9277
Tanya <tanyaborisenko94@gmail.com>
2022-01-12 23:55:33 +0300

    Second practical task

--- EpamHomeWork/Program.cs
+++ EpamHomeWork/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Linq;
 
 namespace EpamHomeWork
 {
-    class Program
+    class ConvertNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type number and press Enter");
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type another number from 2 to 20 and press Enter");
+            var baseNumber = int.Parse(Console.ReadLine());
+
+            if (baseNumber < 2 || baseNumber > 20)
+            {
+                throw new ArgumentException("Please type number from 2 to 20.");
+            }
+
+            string conversion = "";
+
+            while (number != 0 && baseNumber >= 2 && baseNumber <= 20)
+            {
+                conversion += Convert.ToString(number % baseNumber);
+                number = number / baseNumber;
+            }
+
+            var conversion2 = conversion.ToArray().Reverse();
+            Console.WriteLine("Your convert number :");
+            Console.Write(string.Join("", conversion2));
         }
     }
 }
